c77ff9fa62b53dc507d2ca8c76dab003866d4199
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-04-26 22:23:25 +0300

    Add code for computer choice

--- rock_paper_scissors/JS_file.js
+++ rock_paper_scissors/JS_file.js
@@ -1,1 +1,13 @@
 // JS initial commit
+
+function getComputerChoice() {
+  const calcFormula = Math.floor(Math.random() * 3);
+
+  if (calcFormula === 0) {
+    console.log("rock");
+  } else if (calcFormula === 1) {
+    console.log("paper");
+  } else {
+    console.log("scissors");
+  }
+}
