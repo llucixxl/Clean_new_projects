79acb110ade843c186a66b4a3ba2eb40c80522c4
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-06-23 19:29:37 +0300

    add new comm for rps-ui branch test commit

--- rock_paper_scissors/JS_file.js
+++ rock_paper_scissors/JS_file.js
@@ -159,6 +159,6 @@ Human win! Scissors beat paper!`
   }
 }
 
-// End of winner announce in the browser's console
+// End of winner announce in the browser's console // test commit for rps-ui branch
 
 playGame();
